76f8ce9a527fb30ff9855ea260f29825a9f2471d
yesol <maniac970430@naver.com>
2023-09-24 12:08:03 +0900

    페이지 구현: 홈(Home)-작성 월에 맞는 일기 리스트 화면에 표시

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import Header from "../components/Header";
 import Button from "../components/Button";
@@ -7,8 +7,32 @@ import { DiaryContext } from "../context/diary-context";
 const Home = () => {
   const diaryCtx = useContext(DiaryContext);
   const [curDate, setCurDate] = useState(new Date());
+  const [data, setData] = useState([]);
 
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  useEffect(() => {
+    if (diaryCtx.diary) {
+      //현재 년월의 첫번째 날
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+
+      //다음 달 0일까지
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      //이번 달 일기를 추리기
+      setData(
+        diaryCtx.diary.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryCtx.diary, curDate]); //일기가 변화하거나 현재 시간이 변화할때 리렌더링됨
 
   const decreaseMonthHandler = () => {
     setCurDate(
@@ -23,17 +47,16 @@ const Home = () => {
   };
 
   return (
-    <>
+    <div>
       <Header
         headText={headText}
         leftBtn={<Button text="◀" onClick={decreaseMonthHandler} />}
         rightBtn={<Button text="▶" onClick={increaseMonthHandler} />}
       />
-      <h1>Home</h1>
-      {diaryCtx.diary.map((it) => (
-        <div>{it.content}</div>
+      {data.map((it) => (
+        <div key={it.id}>{it.content}</div>
       ))}
-    </>
+    </div>
   );
 };
 export default Home;
