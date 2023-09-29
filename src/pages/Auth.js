3e6f3f7fb05733b6eeea67575b3d119e8ad1f4f2
yesol <maniac970430@naver.com>
2023-09-29 21:22:28 +0900

    :zap:회원가입 기능 후처리
    사용자에게 회원가입 성공을 알리기, 사용자가 입력한 값을 form에서 지우기

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase-config";
 
@@ -12,12 +12,17 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const emailRef = useRef();
+  const pwdRef = useRef();
+
   const navigate = useNavigate();
 
   const signIn = async () => {
-    console.log(email, password);
     try {
       const user = await createUserWithEmailAndPassword(auth, email, password);
+      alert("회원가입에 성공하셨습니다! 로그인 해주세요.");
+      emailRef.current.value = "";
+      pwdRef.current.value = "";
       console.log(user);
     } catch (err) {
       console.log(err);
@@ -44,12 +49,14 @@ const Auth = () => {
           <input
             type="email"
             placeholder="이메일"
+            ref={emailRef}
             onChange={(e) => setEmail(e.target.value)}
           />
           <h4>비밀번호</h4>
           <input
             type="password"
             placeholder="비밀번호"
+            ref={pwdRef}
             onChange={(e) => setPassword(e.target.value)}
           />
           <div className="btn-field">
